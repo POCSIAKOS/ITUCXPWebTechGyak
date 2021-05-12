dfe22b0e96f45e2b1e72fee5da40e790a384f255
POCSIAKOS <pocsiakos@outlook.com>
2021-05-12 09:24:47 +0200

    added new folder

---
+++ ITUCXP_04.28/feldolgozo1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="hu">
+ <head>
+    <title>Űrlap vezérlése új elemekkel</title>
+	<meta charset="UTF-8">
+  
+ </head>
+
+ <body>
+
+ <font style="font-size:18pt">
+ <h2> Űrlap vezérlése új elemekkel </h2>
+
+ <?php
+
+ print ("Név: " .$_POST['nev'] . "<br>");
+
+ print ("PIN kód: " .$_POST['kod'] . "<br>");
+
+
+ ?>
+
+ <a href="urlap3.html">vissza az űrlapra...</a>
+
+ </font>
+  
+ </body>
+</html>
---
+++ ITUCXP_04.28/feldolgozo2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="hu">
+ <head>
+    <title>Űrlap vezérlése új elemekkel</title>
+	<meta charset="UTF-8">
+  
+ </head>
+
+ <body>
+
+ <font style="font-size:18pt">
+ <h2> Űrlap vezérlése új elemekkel </h2>
+
+ <?php
+
+ print ("Név: " .$_GET['nev'] . "<br>");
+
+ print ("Pin kód: " .$_GET['kod'] . "<br>");
+
+ ?>
+
+ <a href="urlap3.html">vissza az űrlapra...</a>
+
+ </font>
+  
+ </body>
+</html>
